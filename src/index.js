87371c9aee9c6624e839823043d216aa6f1f9735
Luyanda <luyanda.s.zuma@gmail.com>
2025-07-08 14:36:34 +0200

    Updated index.html and index.js
    Added a wait message for the user in index.js to improve user experience.

--- src/index.js
+++ src/index.js
@@ -13,6 +13,12 @@ function callApi(event) {
   let prompt = `User instructions: Generate a product description about ${userProductName.value} with a word count of 80 to 100 words.`;
   let context = `You are a marketing specialist who writes amazing product descriptions. Use the provided product details ${userProductDetails.value} to create an attractive and concise product description. Follow user instructions.`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  new Typewriter("#description", {
+    strings: `⏳Generating...`,
+    autoStart: true,
+    delay: 20,
+  });
   axios.get(apiUrl).then(generateDescription);
 }
 
@@ -20,7 +26,7 @@ function generateDescription(response) {
   new Typewriter("#description", {
     strings: response.data.answer,
     autoStart: true,
-    delay: 25,
+    delay: 20,
     cursor: "",
   });
 }
