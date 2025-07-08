add8f0a5ed4bf9b8850c64822f59a45a83a4bd76
Luyanda <luyanda.s.zuma@gmail.com>
2025-07-08 13:55:13 +0200

    Updated index.html and index.js files

--- src/index.js
+++ src/index.js
@@ -10,8 +10,8 @@ function callApi(event) {
   let userProductDetails = document.querySelector(
     "#user-instructions-product-details"
   );
-  let prompt = `Generate a product description about ${userProductName.value} `;
-  let context = `Use the provided product details ${userProductDetails.value} to create an attractive and concise 100 words product description.`;
+  let prompt = `Generate a product description about ${userProductName.value} with a word count between 80 to 100 words.`;
+  let context = `Use the provided product details ${userProductDetails.value} to create an attractive and concise product description.`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=a3f9ff86f4t0ab9cbe395o06803b6fc9`;
   axios.get(apiUrl).then(generateDescription);
 }
