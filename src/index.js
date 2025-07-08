cb7e5f288430730ea21d7f29ef7d994e0a53dbbf
Luyanda <luyanda.s.zuma@gmail.com>
2025-07-08 13:09:20 +0200

    Updated index.html, styles.css and index.js
    Added ids in index.html. Updated styling in styles.css and added callApi function in index.js.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 let productFormElement = document.querySelector("#form-group");
-productFormElement.addEventListener("submit", generateDescription);
+productFormElement.addEventListener("submit", callApi);
 
-function generateDescription(event) {
+function callApi(event) {
   event.preventDefault();
+  let apiKey = "a3f9ff86f4t0ab9cbe395o06803b6fc9";
+  let userProductName = document.querySelector(
+    "#user-instructions-product-name"
+  );
+  let userProductDetails = document.querySelector(
+    "#user-instructions-product-details"
+  );
+  let prompt = `Generate a product description about ${userProductName.value} `;
+  let context = `Use the provided product details ${userProductDetails.value} to create an attractive and concise 100 words product description.`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=a3f9ff86f4t0ab9cbe395o06803b6fc9`;
+  axios.get(apiUrl).then(generateDescription);
+}
+
+function generateDescription(response) {
   new Typewriter("#description", {
-    strings: "Brown wooden truck toy...",
+    strings: response.data.answer,
     autoStart: true,
-    delay: 50,
+    delay: 30,
     cursor: "",
   });
 }
